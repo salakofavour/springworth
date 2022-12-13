cfe4ea702e7b61de033647111dcb6d83638e7b63
vedpra55 <vedna400@gmail.com>
2022-12-13 11:54:05 +0530

    changes

--- src/pages/api/chat/sendMessageEmail.js
+++ src/pages/api/chat/sendMessageEmail.js
@@ -2,22 +2,24 @@ const nodemailer = require("nodemailer");
 
 export default async function handler(req, res) {
   const data = req.body;
-  const test = "iiabmfviewjxmgrt";
-  const test2 = "neby scjs pbei mjwt";
+
+  const test = "zbbpgvyoizddqtgg";
+  const passw = "bbvh hors bgbq pxjm";
+  const email = "noreply.springworthbooks@gmail.com";
 
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: "noreply.springworthbooks@gmail.com", // generated ethereal user
-      pass: "bbvh hors bgbq pxjm", // generated ethereal password
+      user: email, // generated ethereal user
+      pass: passw, // generated ethereal password
     },
   });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: `${data.senderEmail}`, // sender address
+    from: `"SPRINGWORTH BOOKS" ${data.senderEmail}`, // sender address
     to: `${data.reciverEmail}`, // list of receivers
     subject: "You have a new message in Springworth Books", // Subject line
     text: data.message, // plain text body
